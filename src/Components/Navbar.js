5ab8ddfbac56d131b23fd3f289d6718a3680b93f
Priyadharshan <91417662+Priyadharshan0903@users.noreply.github.com>
2023-01-24 18:32:16 +0530

    Add files via upload
    Make sure you are downloading Node modules after unzipping this file.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+function Navbar() {
+  return (
+       <div id="nav">
+            <div id='nav-title'>
+                 <img src='./Images/Marvel_comics-logo.png' />
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
